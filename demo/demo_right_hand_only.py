4b1029925ac5b0191a518a9edbebdd07e92548dd
Gyeongsik Moon <mks0601@gmail.com>
2024-03-17 04:17:42 -0400

    Update demo_right_hand_only.py

--- demo/demo_right_hand_only.py
+++ demo/demo_right_hand_only.py
@@ -28,23 +28,7 @@ from utils.preprocessing import load_img, process_bbox, generate_patch_image, ge
 from utils.vis import vis_keypoints_with_skeleton, save_obj, render_mesh_orthogonal
 from utils.mano import mano
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--gpu', type=str, dest='gpu_ids')
-    args = parser.parse_args()
-
-    assert args.gpu_ids, "Please set proper gpu ids"
-
-    if '-' in args.gpu_ids:
-        gpus = args.gpu_ids.split('-')
-        gpus[0] = int(gpus[0])
-        gpus[1] = int(gpus[1]) + 1
-        args.gpu_ids = ','.join(map(lambda x: str(x), list(range(*gpus))))
-    
-    return args
-
-args = parse_args()
-cfg.set_args(args.gpu_ids)
+cfg.set_args()
 cudnn.benchmark = True
 
 # snapshot load
@@ -71,6 +55,7 @@ img_path_list = glob(osp.join(input_img_path, '*.jpg')) + glob(osp.join(input_im
 
 # for each input image
 for img_path in tqdm(img_path_list):
+    img_path = osp.join(input_img_path, '%06d.png' % frame_idx)
     file_name = img_path.split('/')[-1][:-4]
     
     # load image and make its aspect ratio follow cfg.input_img_shape
@@ -90,99 +75,57 @@ for img_path in tqdm(img_path_list):
     with torch.no_grad():
         out = model(inputs, targets, meta_info, 'test')
     
-    # check IoU between boxes of two hands
-    rhand_bbox = out['rhand_bbox'].cpu().numpy()[0]
-    lhand_bbox = out['lhand_bbox'].cpu().numpy()[0]
-    iou = get_iou(rhand_bbox, lhand_bbox, 'xyxy')
-    is_th = False
-
-    # for each right and left hand
-    vis_box = original_img.copy()[:,:,::-1]
-    vis_skeleton = original_img.copy()[:,:,::-1]
-    prev_depth = None
-    render_out = torch.flip(torch.from_numpy(original_img).float().cuda()[None,:,:,:], [3]) # batch_size, img_height, img_width, 3
-    rroot_cam = out['rroot_cam'].cpu().numpy()[0] # 3D position of the right hand root joint (wrist)
-    rel_trans = out['rel_trans'].cpu().numpy()[0] # 3D relative translation between two hands
-    for h in ('right', 'left'):
-      
-        if h == 'left':
-          continue
-          
-        # get outputs
-        hand_bbox = out[h[0] + 'hand_bbox'].cpu().numpy()[0].reshape(2,2) # xyxy
-        hand_bbox_conf = float(out[h[0] + 'hand_bbox_conf'].cpu().numpy()[0]) # bbox confidence
-        joint_img = out[h[0] + 'joint_img'].cpu().numpy()[0] # 2.5D pose
-        mesh = out[h[0] + 'mano_mesh_cam'].cpu().numpy()[0] # root-relative mesh
-        root_pose = out[h[0] + 'mano_root_pose'].cpu().numpy()[0] # MANO root pose
-        hand_pose = out[h[0] + 'mano_hand_pose'].cpu().numpy()[0] # MANO hand pose
-        shape = out[h[0] + 'mano_shape'].cpu().numpy()[0] # MANO shape parameter
-        root_cam = out[h[0] + 'root_cam'].cpu().numpy()[0] # 3D position of the root joint (wrist)
+    # get outputs
+    hand_bbox = out['rhand_bbox'].cpu().numpy()[0].reshape(2,2) # xyxy
+    joint_img = out['rjoint_img'].cpu().numpy()[0] # 2.5D pose
+    mesh = out['rmano_mesh_cam'].cpu().numpy()[0] # root-relative mesh
+    root_pose = out['rmano_root_pose'].cpu().numpy()[0] # MANO root pose
+    hand_pose = out['rmano_hand_pose'].cpu().numpy()[0] # MANO hand pose
+    shape = out['rmano_shape'].cpu().numpy()[0] # MANO shape parameter
+    root_cam = out['rroot_cam'].cpu().numpy()[0] # 3D position of the root joint (wrist)
+    
+    # translate
+    mesh = mesh + root_cam
+    render_focal = out['render_rfocal'].clone()
+    render_princpt = out['render_rprincpt'].clone()
         
-        # use rel_trans only when two-hand cases
-        if is_th:
-            if h == 'right':
-                mesh = mesh + rroot_cam[None,:]
-            else:
-                mesh = mesh + rroot_cam[None,:] + rel_trans[None,:]
-            render_focal = out['render_focal'].clone()
-            render_princpt = out['render_princpt'].clone()
-        else:
-            mesh = mesh + root_cam
-            render_focal = out['render_' + h[0] + 'focal'].clone()
-            render_princpt = out['render_' + h[0] + 'princpt'].clone()
-            
-        # warp from cfg.input_img_shape to the orignal image space
-        render_focal[:,0] = render_focal[:,0] / cfg.input_img_shape[1] * bbox[2]
-        render_focal[:,1] = render_focal[:,1] / cfg.input_img_shape[0] * bbox[3]
-        render_princpt[:,0] = render_princpt[:,0] / cfg.input_img_shape[1] * bbox[2] + bbox[0]
-        render_princpt[:,1] = render_princpt[:,1] / cfg.input_img_shape[0] * bbox[3] + bbox[1]
+    # warp from cfg.input_img_shape to the orignal image space
+    render_focal[:,0] = render_focal[:,0] / cfg.input_img_shape[1] * bbox[2]
+    render_focal[:,1] = render_focal[:,1] / cfg.input_img_shape[0] * bbox[3]
+    render_princpt[:,0] = render_princpt[:,0] / cfg.input_img_shape[1] * bbox[2] + bbox[0]
+    render_princpt[:,1] = render_princpt[:,1] / cfg.input_img_shape[0] * bbox[3] + bbox[1]
 
-        # bbox save
-        hand_bbox[:,0] = hand_bbox[:,0] / cfg.input_body_shape[1] * cfg.input_img_shape[1]
-        hand_bbox[:,1] = hand_bbox[:,1] / cfg.input_body_shape[0] * cfg.input_img_shape[0]
-        hand_bbox_xy1 = np.concatenate((hand_bbox, np.ones_like(hand_bbox[:,:1])),1)
-        hand_bbox = np.dot(bb2img_trans, hand_bbox_xy1.transpose(1,0)).transpose(1,0)
-        if h == 'right':
-            color = (255,0,255) # purple
-        else:
-            color = (102,255,102) # green
-        vis_box = cv2.rectangle(vis_box.copy(), (int(hand_bbox[0,0]), int(hand_bbox[0,1])), (int(hand_bbox[1,0]), int(hand_bbox[1,1])), color, 3)
+    # bbox save
+    hand_bbox[:,0] = hand_bbox[:,0] / cfg.input_body_shape[1] * cfg.input_img_shape[1]
+    hand_bbox[:,1] = hand_bbox[:,1] / cfg.input_body_shape[0] * cfg.input_img_shape[0]
+    hand_bbox_xy1 = np.concatenate((hand_bbox, np.ones_like(hand_bbox[:,:1])),1)
+    hand_bbox = np.dot(bb2img_trans, hand_bbox_xy1.transpose(1,0)).transpose(1,0)
+    color = (255,0,255) # purple
+    vis_box = cv2.rectangle(original_img[:,:,::-1].copy(), (int(hand_bbox[0,0]), int(hand_bbox[0,1])), (int(hand_bbox[1,0]), int(hand_bbox[1,1])), color, 3)
 
-        # 2D skeleton
-        joint_img_xy1 = np.concatenate((joint_img[:,:2], np.ones_like(joint_img[:,:1])),1)
-        joint_img = np.dot(bb2img_trans, joint_img_xy1.transpose(1,0)).transpose(1,0)
-        if h == 'right':
-            color = (255,0,255) # purple
-        else:
-            color = (102,255,102) # green
-        vis_skeleton = vis_keypoints_with_skeleton(vis_skeleton, joint_img, mano.sh_skeleton, color)
+    # 2D skeleton
+    joint_img_xy1 = np.concatenate((joint_img[:,:2], np.ones_like(joint_img[:,:1])),1)
+    joint_img = np.dot(bb2img_trans, joint_img_xy1.transpose(1,0)).transpose(1,0)
+    color = (255,0,255) # purple
+    vis_skeleton = vis_keypoints_with_skeleton(original_img[:,:,::-1].copy(), joint_img, mano.sh_skeleton, color)
 
-        # save mesh
-        save_obj(mesh, mano.face[h], osp.join(mesh_save_path, file_name + '_' + h + '.obj'))
+    # save mesh
+    save_obj(mesh, mano.face['right'], osp.join(mesh_save_path, file_name + '_right.obj'))
 
-        # save MANO parameters
-        with open(osp.join(param_save_path, file_name + '_' + h + '.json'), 'w') as f:
-            if h == 'right':
-                json.dump({'root_pose': root_pose.tolist(), 'hand_pose': hand_pose.tolist(), 'shape': shape.tolist(), 'root_trans': [0,0,0]}, f)
-            else:
-                 json.dump({'root_pose': root_pose.tolist(), 'hand_pose': hand_pose.tolist(), 'shape': shape.tolist(), 'root_trans': rel_trans.tolist()}, f)
+    # save MANO parameters
+    with open(osp.join(param_save_path, file_name + '_right.json'), 'w') as f:
+        json.dump({'root_pose': root_pose.tolist(), 'hand_pose': hand_pose.tolist(), 'shape': shape.tolist(), 'root_trans': root_cam.tolist(), 'focal_ortho': render_focal.tolist(), 'princpt_ortho': render_princpt.tolist()}, f)
 
-        # render
-        with torch.no_grad():
-            mesh = torch.from_numpy(mesh[None,:,:]).float().cuda()
-            face = torch.from_numpy(mano.face[h][None,:,:].astype(np.int32)).cuda()
-            render_cam_params = {'focal': render_focal, 'princpt': render_princpt}
-            rgb, depth = render_mesh_orthogonal(mesh, face, render_cam_params, (img_height,img_width), h)
-        valid_mask = (depth > 0)
-        if prev_depth is None:
-            render_mask = valid_mask.float()
-            render_out = rgb * render_mask + render_out * (1 - render_mask)
-            prev_depth = depth
-        else:
-            render_mask = (valid_mask * (((depth < prev_depth) + (prev_depth <= 0)) > 0)).float()
-            render_out = rgb * render_mask + render_out * (1 - render_mask)
-            prev_depth = depth * render_mask + prev_depth * (1 - render_mask)
- 
+    # render
+    with torch.no_grad():
+        mesh = torch.from_numpy(mesh[None,:,:]).float().cuda()
+        face = torch.from_numpy(mano.face['right'][None,:,:].astype(np.int32)).cuda()
+        render_cam_params = {'focal': render_focal, 'princpt': render_princpt}
+        rgb, depth = render_mesh_orthogonal(mesh, face, render_cam_params, (img_height,img_width), 'right')
+        fg, is_fg = rgb[0].cpu().numpy(), (depth[0].cpu().numpy() > 0)
+        bg = original_img[:,:,::-1].copy()
+        render_out = fg * is_fg + bg * (1 - is_fg)
+
     # save box
     cv2.imwrite(osp.join(render_save_path, file_name + '_box.jpg'), vis_box)
 
@@ -190,4 +133,5 @@ for img_path in tqdm(img_path_list):
     cv2.imwrite(osp.join(render_save_path, file_name + '_skeleton.jpg'), vis_skeleton)
    
     # save render
-    cv2.imwrite(osp.join(render_save_path, file_name + '_mesh.jpg'), render_out[0].cpu().numpy())
+    cv2.imwrite(osp.join(render_save_path, file_name + '_mesh.jpg'), render_out)
+
