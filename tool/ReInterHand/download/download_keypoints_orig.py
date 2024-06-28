6314d5ee84d3850d836c92ffc66a40a235e4cf11
mks0601 <mks0601@gmail.com>
2024-06-28 00:05:14 +0000

    add copyright header

--- tool/ReInterHand/download/download_keypoints_orig.py
+++ tool/ReInterHand/download/download_keypoints_orig.py
@@ -1,3 +1,10 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+# 
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# 
+
 import os
 import os.path as osp
 from tqdm import tqdm
